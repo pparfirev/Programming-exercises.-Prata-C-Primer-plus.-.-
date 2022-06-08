26570fcf9243550936b563152ac92aa67b5b7ddd
pparfirev <99344548+pparfirev@users.noreply.github.com>
2022-06-08 16:20:23 +0300

    Add files via upload
    Reconfig Ch.2

---
+++ Chapter 2/2.2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Напишите программу, выводящую ваше имя и адрес. */
+
+int main()
+{
+    printf("Name: Pavel Parfirev\n");
+    printf("Adress: p.parfirev@yandex.ru\n");
+
+    return 0;
+}
