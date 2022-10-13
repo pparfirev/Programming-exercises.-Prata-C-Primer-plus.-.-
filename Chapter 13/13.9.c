808539c91aecba4ab770c06b3fd25ed45c5e76be
pparfirev <99344548+pparfirev@users.noreply.github.com>
2022-10-13 21:30:58 +0300

    Add files via upload

---
+++ Chapter 13/13.9.c
@@ -0,0 +1,42 @@
+/* ћодифицируйте программу в листинге 13.3, чтобы каждое слово было пронумеровано
+в соответствии с пор€дком его добавлени€ в список, начина€ с 1.
+ѕозаботьтесь о том, чтобы при втором запуске программы нова€ нумераци€
+слов начиналась с того места, где была закончена пумераци€ при предыдущем
+запуске. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 41
+static int count = 1; // newline counting variable
+int main()
+{
+    FILE *fp;
+    char words[MAX];
+    char buf[MAX];
+    if((fp = fopen("wordy.txt", "a+")) == NULL)
+    {
+        fprintf(stdout, "Can't open file \"wordy.txt\".\n");
+        exit(EXIT_FAILURE);
+    }
+     while (!feof(fp)) // first counting lines in opened file
+    {
+        if (fgetc(fp) == '\n') // if we have newline symbol
+            count++; // increase the counter
+    }
+    puts("Enter words for adding in file; for exit enter symbol # at the start of the line.");
+    while((fscanf(stdin, "%40s", words) == 1) && (words[0] != '#'))
+    {
+        fprintf(fp, "%d. %s\n", count++, words); // and increase the counter here after every newline
+    }
+    puts("File contents: ");
+    rewind(fp); // Return to the begining of the file
+   // count = 1;
+    while(fscanf(fp, "%s", words) == 1)
+    {
+        puts(words);
+    }
+    puts("Done!");
+    if(fclose(fp) != 0)
+        fprintf(stderr, "Error closing file\n");
+    return 0;
+}
