3eaf9a609f19cf0d2ce89f54c40a41a47c4d9afd
pparfirev <99344548+pparfirev@users.noreply.github.com>
2022-11-04 16:31:00 +0300

    Create Chapter 14 branch and added 14.1

---
+++ Chapter 14/14.1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct month
+{
+    char name[20];
+    char abv[3];
+    int days;
+    int numb;
+};
+struct month months [12] =
+{
+    {"January", "JAN", 31, 1},
+    {"February", "FEB", 28, 2},
+    {"March", "MAR", 31, 3},
+    {"April", "APR", 30, 4},
+    {"May", "MAY", 31, 5},
+    {"June", "JUN", 30, 6},
+    {"July", "JUL", 31, 7},
+    {"August", "AUG", 31, 8},
+    {"September", "SEP", 30, 9},
+    {"October", "OCT", 31, 10},
+    {"November", "NOV", 30, 11},
+    {"December", "DEC", 31, 12},
+};
+int days_m(char *mon);
+int main()
+{
+    char ent_month[20];
+    puts("Enter the name of month to get number of days since start of the year: ");
+    scanf("%s", &ent_month);
+    int days = days_m(ent_month);
+    //puts(ent_month);
+    printf("Total days: %d.\n", days);
+    return 0;
+}
+int days_m(char *mon)
+{
+    int days = 0;
+    int equal = 0; // pointer for month is finding
+    for(int n = 0; n < 13; n++)
+    {
+        days += months[n].days;
+       if(!(strcmp(mon, months[n].name)))
+       {
+           equal = 1;
+            break;
+       }
+    }
+    if(!equal) // if month not finding enter the error message
+    {
+        puts("Wrong month.");
+        days = 0;
+    }
+    return days;
+}
