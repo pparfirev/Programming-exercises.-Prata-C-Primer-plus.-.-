539f29dabe647270954ca6bf45c29a12dee16e2f
pparfirev <99344548+pparfirev@users.noreply.github.com>
2023-01-06 19:23:28 +0300

    Add files via upload

---
+++ Chapter 15/15.3.c
@@ -0,0 +1,46 @@
+/* Напишите функцию, которая принимает аргумент типа int и возвращает количество
+включенных битов в нем. Протестируйте функцию в какой-нибудь программе. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <string.h>
+int onbits(int n);
+char *itobs(int, char *);
+int main()
+{
+    int number;
+    char bin_str[CHAR_BIT *sizeof(int) + 1];
+    printf("Enter the integer number to analyze or enter non numeric value to exit: \n");
+        while(scanf("%d", &number) == 1)
+    {
+        itobs(number, bin_str);
+        printf("%d has %d enabled bits", number, onbits(number));
+        //show_bstr(bin_str);
+        putchar('\n');
+    }
+    return 0;
+}
+char *itobs(int n, char *ps)
+{
+    int i;
+    const static int size = CHAR_BIT * sizeof(int);
+    for(i = size - 1; i >= 0; i--, n >>= 1)
+    {
+        ps[i] = (01 & n) + '0'; //ASCII intepretation
+    }
+    ps[size] = '\0';
+    return ps;
+}
+int onbits(int n)
+{
+    static const int size = CHAR_BIT * sizeof(int);
+    int ans = 0;
+    int i;
+    for(i = 0; i < size; i++, n >>= 1)
+    {
+        if((1 & n) == 1)
+            ans++;
+    }
+    return ans;
+}
