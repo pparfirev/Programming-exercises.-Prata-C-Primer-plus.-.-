e16f7c7cac70e01ff054e41737dc9212195ec97d
pparfirev <99344548+pparfirev@users.noreply.github.com>
2023-01-06 19:25:54 +0300

    Update 15.3.c

--- Chapter 15/15.3.c
+++ Chapter 15/15.3.c
@@ -6,7 +6,6 @@
 #include <limits.h>
 #include <string.h>
 int onbits(int n);
-char *itobs(int, char *);
 int main()
 {
     int number;
@@ -21,17 +20,7 @@ int main()
     }
     return 0;
 }
-char *itobs(int n, char *ps)
-{
-    int i;
-    const static int size = CHAR_BIT * sizeof(int);
-    for(i = size - 1; i >= 0; i--, n >>= 1)
-    {
-        ps[i] = (01 & n) + '0'; //ASCII intepretation
-    }
-    ps[size] = '\0';
-    return ps;
-}
+
 int onbits(int n)
 {
     static const int size = CHAR_BIT * sizeof(int);
